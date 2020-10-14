62dc3c9ac861b955ded331f42dfa5ff2f4f1ebec
durakin <albertnepomnyashiy@gmail.com>
2020-10-14 10:58:19 +0700

    lab3 code is ready

---
+++ Lab3/main.c
@@ -0,0 +1,156 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+enum Constants
+{
+    INPUT_SIZE = 100
+};
+
+void SwapInt(int* a, int* b)
+{
+    int box;
+    box = *b;
+    *b = *a;
+    *a = box;
+}
+
+int CycleInputInt(char* stringToOutput, bool(* pChecker)(int))
+{
+    int number;
+    int position;
+    char input[INPUT_SIZE];
+
+    while (true)
+    {
+        printf("%s\n", stringToOutput);
+        fflush(stdout);
+        char* fgetsRet = fgets(input, INPUT_SIZE, stdin);
+        if (fgetsRet == NULL)
+        {
+            printf("Wrong format!\n");
+            continue;
+        }
+        int inputLength = strlen(input) - 1;
+        input[inputLength] = '\0';
+        int sscanfRet = sscanf(input, "%d%n", &number, &position);
+        if (position != inputLength)
+        {
+            printf("Wrong format!\n");
+            continue;
+        }
+        if (pChecker && !pChecker(number))
+        {
+            printf("Wrong format!\n");
+            continue;
+        }
+        if (sscanfRet == 1) break;
+        printf("Wrong format!\n");
+    }
+    return number;
+}
+
+bool ArraySizeInputChecker(int arraySize)
+{
+    return arraySize > 0;
+}
+
+bool AnyIntInputChecker(int _)
+{
+    return true;
+}
+
+typedef struct
+{
+    int* content;
+    int size;
+} DynArray;
+
+void PrintArray(DynArray* object)
+{
+    for (int i = 0; i < object->size; i++)
+    {
+        printf("%d ", object->content[i]);
+    }
+    printf("\n");
+}
+
+void ArrayCopy(DynArray* origin, DynArray* object)
+{
+    free(object->content);
+    object->size = origin->size;
+    object->content = (int*) malloc(object->size * sizeof(int));
+    for (int i = 0; i < object->size; i++)
+    {
+        object->content[i] = origin->content[i];
+    }
+}
+
+void InsertionSort(DynArray* object)
+{
+    for (int i = 0; i < object->size; i++)
+    {
+        int box;
+        box = object->content[i];
+        int j;
+        j = 0;
+        while (object->content[j] < box)
+        {
+            j++;
+        }
+        for (int k = i - 1; k >= j; k--)
+        {
+            object->content[k + 1] = object->content[k];
+        }
+        object->content[j] = box;
+    }
+}
+
+void SelectionSort(DynArray* object)
+{
+    for (int i = 0; i < object->size; i++)
+    {
+        int jBox;
+        jBox = i;
+        int box;
+        box = object->content[i];
+        for (int j = i; j < object->size; j++)
+        {
+            if (object->content[j] < box)
+            {
+                jBox = j;
+                box = object->content[j];
+            }
+        }
+        SwapInt(&(object->content[i]), &(object->content[jBox]));
+    }
+}
+
+int main()
+{
+    DynArray object;
+    object.size = CycleInputInt("Enter size of array", ArraySizeInputChecker);
+    object.content = (int*) malloc(object.size * sizeof(int));
+    printf("Enter elements, one by one");
+    for (int i = 0; i < object.size; i++)
+    {
+        object.content[i] = CycleInputInt("Enter next element",
+                                          AnyIntInputChecker);
+    }
+
+    printf("Origin array:\n");
+    PrintArray(&object);
+    printf("Insertion sorted:\n");
+    DynArray coppy;
+    ArrayCopy(&object, &coppy);
+    InsertionSort(&coppy);
+    PrintArray(&coppy);
+    printf("Selection sorted:\n");
+    ArrayCopy(&object, &coppy);
+    SelectionSort(&coppy);
+    PrintArray(&coppy);
+
+    free(object.content);
+    free(coppy.content);
+}
